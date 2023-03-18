104242253979e17e790f8ad8879aa6fe4dafb5ac
Titan Jamison <jamisontitan@gmail.com>
2023-03-18 16:52:57 -0700

    moving to section 5

--- test.c
+++ test.c
@@ -1,19 +1,42 @@
 #include <gtk/gtk.h>
 
-static void click_cb (GtkButton *btn) {
-	g_print("Clickideedoo \n");
+static void click1_cb (GtkButton *btn) {
+	const char * s;
+
+	s = gtk_button_get_label(btn);
+	if(g_strcmp0 (s, "Hello.") == 0) {
+		gtk_button_set_label(btn, "Good-bye.");
+	} else {
+		gtk_button_set_label(btn, "Hello.");
+	}
+}
+
+static void click2_cb (GtkButton *btn, GtkWindow *win) {
+	gtk_window_destroy(win);
 }
 
 static void app_activate(GApplication *app) {
 	GtkWidget *win;
+	GtkWidget *box;
+	GtkWidget *btn1;
+	GtkWidget *btn2;
+
 	win = gtk_application_window_new (GTK_APPLICATION (app));
-	GtkWidget *btn;
 	gtk_window_set_title (GTK_WINDOW (win), "Test");
 	gtk_window_set_default_size (GTK_WINDOW (win), 400, 300);
 
-	btn = gtk_button_new_with_label("I fucken dare you");
-	gtk_window_set_child(GTK_WINDOW (win), btn);
-	g_signal_connect(btn, "clicked", G_CALLBACK(click_cb), NULL);
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	gtk_box_set_homogeneous(GTK_BOX (box), TRUE);
+	gtk_window_set_child(GTK_WINDOW (win), box);
+
+	btn1 = gtk_button_new_with_label("Hello.");
+	g_signal_connect(btn1, "clicked", G_CALLBACK (click1_cb), NULL);
+
+	btn2 = gtk_button_new_with_label("Close");
+	g_signal_connect(btn2, "clicked", G_CALLBACK (click2_cb), win);
+
+	gtk_box_append(GTK_BOX (box), btn1);
+	gtk_box_append(GTK_BOX (box), btn2);
 
 	gtk_window_present (GTK_WINDOW (win));
 }
